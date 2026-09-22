80c8127bcf258965b90a7780e877efc4a59faf47
kita0905game-cyber <kita0905.game@gmail.com>
2026-09-22 12:10:15 +0900

    Use type-only FormEvent import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useEffect, useState } from 'react';
+import { useEffect, useState, type FormEvent } from 'react';
 import LifeQuestGame from './game/LifeQuestGame';
 import {
   bootstrapCloudSave,
